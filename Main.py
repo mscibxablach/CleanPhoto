ca6cd8a89c95940085a675412d8e220dea82299a
msciborski <michal.sciborski@student.put.poznan.pl>
2020-01-10 13:51:18 +0100

    Get continous chunks

--- Main.py
+++ Main.py
@@ -115,6 +115,31 @@ def manual_threshold(image):
     return image_copied
 
 
+def get_continous_chunks(array):
+    result = []
+    min_value = min(array)
+    i = 0
+    while i < len(array):
+        value = array[i]
+        if value > min_value:
+            chunk = [i]
+            counter = i + 1
+            for j in range(counter, len(array)):
+                value = array[j]
+                if value > min_value:
+                    chunk.append(j)
+                else:
+                    i = j
+                    result.append(chunk)
+                    break
+            else:
+                continue
+        elif value <= min_value:
+            i += 1
+            continue
+    return result
+
+
 def get_histogram(image):
     result = []
     image_copy = image.copy()
@@ -125,7 +150,6 @@ def get_histogram(image):
         for i in range(rows):
             sum = sum + image[i, j]
         result.append(sum)
-
     plt.plot(result)
     plt.show()
 
@@ -139,7 +163,7 @@ def main():
     cv2.imshow('original image', copied_image)
     copied_image = cv2.cvtColor(copied_image, cv2.COLOR_BGR2GRAY)
     width, height = copied_image.shape
-    copied_image = copied_image[20:, 0:height]
+    copied_image = copied_image[20:width-50, 0:height-50]
     bottom_part = cut_image_on_ox_axis(copied_image, MaskPosition.BOTTOM)
     top_part = cut_image_on_ox_axis(copied_image, MaskPosition.TOP)
 
@@ -153,38 +177,57 @@ def main():
     _, thresholded_bottom = cv2.threshold(manaul_threshold_bottom, 70, 255, cv2.THRESH_BINARY)
     _, thresholded_top = cv2.threshold(manual_threshold_top, 70, 255, cv2.THRESH_BINARY)
 
-    histogram = get_histogram(thresholded_bottom)
-    max_value_in_histogram = max(histogram)
+    # max_value_in_histogram = max(histogram)
 
     ## Dilate of manual threshold image
     # kernel = np.ones((3, 3), np.uint8)
     # dilated_bottom = cv2.dilate(manaul_threshold_bottom, kernel, iterations=3)
     # dilated_top = cv2.dilate(manual_threshold_top, kernel, iterations=3)
 
+    kernel = np.ones((3, 3), np.uint8)
+    # kernel = kernel.astype(np.uint8)
     # kernel = np.ones((5, 5), np.uint8)
-    # morph_open_bottom = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-    # morph_close_bottom = cv2.
+    # print(kernel)
+    # morph_open_bottom = cv2.morphologyEx(thresholded_bottom, cv2.MORPH_OPEN, kernel)
+    # morph_close_bottom = cv2.morphologyEx(morph_open_bottom, cv2.MORPH_CLOSE, kernel)
+    dilated_bottom = cv2.dilate(thresholded_bottom, kernel, iterations=2)
 
-
+    kernel_closing = np.array([[1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1]], np.uint8)
+    morph_close_bottom = cv2.morphologyEx(thresholded_bottom, cv2.MORPH_CLOSE, kernel_closing)
 
     canny_bottom = cv2.Canny(thresholded_bottom, 30, 200)
     canny_top = cv2.Canny(thresholded_top, 30, 200)
 
-    bottom_result = np.concatenate((bottom_part, manaul_threshold_bottom, bilateral_filter_bottom, thresholded_bottom, canny_bottom), axis=0)
+    bottom_result = np.concatenate((bottom_part, manaul_threshold_bottom, bilateral_filter_bottom, thresholded_bottom, dilated_bottom, morph_close_bottom, canny_bottom), axis=0)
     top_result = np.concatenate((top_part, manual_threshold_top, bilateral_filter_top, thresholded_top, canny_top), axis=0)
 
-    contours_bottom, _ = cv2.findContours(canny_bottom.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
+    histogram = get_histogram(dilated_bottom)
+    print(histogram)
+    print(histogram[122])
+    print(histogram[123])
 
     bottom_part = cv2.cvtColor(bottom_part, cv2.COLOR_GRAY2BGR)
+
+    continous_chunks = get_continous_chunks(histogram)
+    for chunk in continous_chunks:
+        color = list(map(lambda x: int(x), list(np.random.choice(range(256), size=3))))
+        chunk_len = len(chunk)
+        start_column = chunk[0]
+        end_column = chunk[-1]
+        cv2.line(bottom_part, (start_column, 0), (start_column, height), color, 1)
+        cv2.line(bottom_part, (end_column, 0), (end_column, height), color, 1)
+
+    # contours_bottom, _ = cv2.findContours(canny_bottom.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
+
     # cv2.drawContours(bottom_part, contours_bottom, -1, [0, 255, 0], 1)
 
     # contours_and_area = list(map(lambda x: (x, cv2.contourArea(x)), contours_bottom))
     # print(contours_and_area)
-    for cnt in contours_bottom:
-        color = list(map(lambda x: int(x), list(np.random.choice(range(256), size=3))))
-        cv2.drawContours(bottom_part, [cnt], 0, color, 1)
+    # for cnt in contours_bottom:
+    #     color = list(map(lambda x: int(x), list(np.random.choice(range(256), size=3))))
+    #     cv2.drawContours(bottom_part, [cnt], 0, color, 1)
 
     cv2.imshow('result', bottom_part)
     cv2.imshow('bottom', bottom_result)
